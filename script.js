af522f7261d9fb10c09836ac520c078ed46014a6
WhatIfNoUsernameNalang <134141922+WhatIfNoUsernameNalang@users.noreply.github.com>
2023-05-20 12:03:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -14,5 +14,5 @@ function checkLogin(event) {
       }
     }
 	function reset(){
-		window.location.href = 'file:///C:/Users/erldy/OneDrive/Documents/CS/Dy%20CS%20Website/Home.html'; 
+		window.location.href = 'https://github.com/WhatIfNoUsernameNalang/NutriSculpt.github.io/blob/77e5e1b494b6d245e244dd63db58914a4f7b3241/Home.html'; 
 	}

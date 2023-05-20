4bc3f2bac5870f8f9cc2cfec0f9f652c413ac208
WhatIfNoUsernameNalang <134141922+WhatIfNoUsernameNalang@users.noreply.github.com>
2023-05-20 12:07:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function checkLogin(event) {
      
       if (username === 'user' && password === '1234') {
     
-        window.location.href = 'https://github.com/WhatIfNoUsernameNalang/NutriSculpt.github.io/blob/77e5e1b494b6d245e244dd63db58914a4f7b3241/Home.html'; 
+        window.location.href = 'Home.html'; 
       } else {
         alert('Invalid username or password. Please try again.');
       }
